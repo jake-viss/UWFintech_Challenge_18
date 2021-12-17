9116ac8ec152650d5814d377a8baadb117ee013a
Jacob_Vissering <jake.vissering@gmail.com>
2021-12-17 10:09:08 -0800

    Code Update - Application Working

--- pychain.py
+++ pychain.py
@@ -51,9 +51,9 @@ import hashlib
 # `amount` attributes
 @dataclass
 class Record:
-    sender: sender
-    receiver: receiver
-    amount: amount
+    sender: str
+    receiver: str
+    amount: float
 
 
 ################################################################################
@@ -192,7 +192,7 @@ if st.button("Add Block"):
     # which is set equal to a `Record` that contains the `sender`, `receiver`,
     # and `amount` values
     new_block = Block(
-        record = Record,
+        record = Record(sender, receiver, amount),
         creator_id=42,
         prev_hash=prev_block_hash
     )
